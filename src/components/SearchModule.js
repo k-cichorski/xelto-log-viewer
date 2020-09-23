2c6add4051ae72f4f642152b793f9183cb9b012a
Kuba <jakub.cichorski@hotmail.com>
2020-09-23 16:14:18 +0200

    SearchModule outline created

---
+++ src/components/SearchModule.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import '../css/SearchModule.css';
+import {KeyboardDatePicker} from '@material-ui/pickers';
+import {ThemeProvider} from '@material-ui/styles';
+import datePickerTheme from '../css/datePickerTheme';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import {IconButton} from '@material-ui/core';
+
+function SearchModule() {
+  const [selectedDateFrom, setSelectedDateFrom] = useState(new Date());
+  const [selectedDateTo, setSelectedDateTo] = useState(new Date());
+  const [inputUserValue, setInputUserValue] = useState('');
+  const [inputStatusValue, setInputStatusValue] = useState('');
+
+  const handleSubmit = e => {
+      e.preventDefault();
+  }
+
+  return (
+      <div className='searchModule'>
+        <form className='searchModule__form'>
+
+            <ThemeProvider theme={datePickerTheme}>
+              <KeyboardDatePicker variant='inline' label='Data od:' autoOk={true}
+                                  format="DD/MM/yyyy" inputVariant='outlined'
+                                  value={selectedDateFrom}
+                                  onChange={setSelectedDateFrom}
+                                  invalidDateMessage='Wpisz poprawną datę'/>
+
+              <KeyboardDatePicker variant='inline' label='Data do:' autoOk={true}
+                                  format="DD/MM/yyyy" inputVariant='outlined'
+                                  value={selectedDateTo}
+                                  onChange={setSelectedDateTo}
+                                  invalidDateMessage='Wpisz poprawną datę'/>
+
+              <TextField variant='outlined' label='Użytkownik' onChange={e => setInputUserValue(e.target.value)}/>
+              <TextField variant='outlined' label='Status' onChange={e => setInputStatusValue(e.target.value)}/>
+
+              <IconButton className='form__searchButton' onClick={e => handleSubmit(e)}>
+                <input type='submit' style={{display: 'none'}}/>
+                <SearchIcon/>
+              </IconButton>
+            </ThemeProvider>
+
+        </form>
+      </div>
+  )
+}
+
+export default SearchModule
+
---
+++ src/css/datePickerTheme.js
@@ -0,0 +1,25 @@
+import { createMuiTheme } from "@material-ui/core";
+export default createMuiTheme({
+    overrides: {
+      MuiPickersToolbar: {
+        toolbar: {
+          backgroundColor: '#726F6F'
+        }
+      },
+      MuiPickersModal: {
+        dialogAction: {
+          color: '#FFFFFF',
+        },
+      },
+      MuiPickersDay: {
+        daySelected: {
+          backgroundColor: '#726F6F'
+        }
+      },
+      MuiPickersYear: {
+        yearSelected: {
+          color: '#726F6F'
+        }
+      },
+    }
+  })
