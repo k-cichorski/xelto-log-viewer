63128cfdebb1d0964a92c07e83c399a4d9f335a6
Kuba <jakub.cichorski@hotmail.com>
2020-09-23 16:29:21 +0200

    fix after revert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import SearchModule from './components/SearchModule';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      
-    </div>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <div className='app'>
+        <div className='app__container'>
+          <SearchModule/>
+        </div>
+      </div>
+    </MuiPickersUtilsProvider>
   );
+  
 }
 
 export default App;
