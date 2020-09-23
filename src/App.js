336b5a4eeb12f880b79696133b8408f8cfd2e0e8
Kuba <jakub.cichorski@hotmail.com>
2020-09-23 16:17:43 +0200

    Revert "SearchModule outline created"
    This reverts commit 2c6add4051ae72f4f642152b793f9183cb9b012a.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
-import SearchModule from './components/SearchModule';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
 
 function App() {
-  
-
   return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-      <div className='app'>
-        <div className='app__container'>
-          <SearchModule/>
-        </div>
-      </div>
-    </MuiPickersUtilsProvider>
+    <div className="App">
+      
+    </div>
   );
-  
 }
 
 export default App;
